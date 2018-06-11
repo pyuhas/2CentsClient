010282dad384e47518750724c2ae6321001f7aff
Pamela <PamelaYuhas@gmail.com>
2018-06-11 16:57:34 -0600

    updated

---
+++ src/components/Activities.vue
@@ -0,0 +1,14 @@
+// All HTML goes here
+<template lang="html">
+
+</template>
+
+//All JavaScript goes here
+<script>
+export default {
+}
+</script>
+
+//All CSS goes here
+<style lang="css">
+</style>
